ae66a0ef5e9f6e9ede9274903dc99f3f4adc2532
Christina Zakarian <christinazakarian@gmail.com>
2017-09-29 01:24:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor(Math.random() * ((100-1) +1) +1)
+  cart.push({[item]: itemPrice})
+  console.log(`${item} has been added to your cart`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  }
+  else if(cart.length === 1){
+    console.log(`In your cart, you have ${cart[0].item} at $${cart[0].itemPrice}.`)
+  }
 }
 
 function total() {
