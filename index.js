b686874a74589a08423215d4fb517ffe56125b70
Christina Zakarian <christinazakarian@gmail.com>
2017-09-29 04:42:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,5 +71,14 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === null) {
+    console.log(`Sorry, we don't have a credit card on file for you.`)
+  }
+  else {
+    var tot = cart.total()
+    console.log(`Your total cost is ${tot}, which will be charged to the card ${cardNumber}.`)
+    while(cart.length > 0) {
+      cart.pop()
+    }
+  }
 }
