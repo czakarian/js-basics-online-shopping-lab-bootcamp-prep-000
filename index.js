6c22045a953e7394d372b3ef7109f36790579348
Christina Zakarian <christinazakarian@gmail.com>
2017-09-29 04:52:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(cardNumber === null) {
+  if(!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
   else {
